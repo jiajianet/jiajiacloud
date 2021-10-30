026213e51556207d6b2e3c05acc211ccf72642a2
jiajianet <69965213+jiajianet@users.noreply.github.com>
2021-10-30 09:11:12 +0800

    20211030 Update JCC

---
+++ jcc/api/HDclass/tet/vid/scripts/config_xml.js
@@ -0,0 +1,66 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:creator="JiaJiaNet" dc:date="2021-08-18 09:08:11 下午" dc:source="Camtasia,21.0.1,chs" dc:title="vid" tscDM:firstFrame="vid_First_Frame.png" tscDM:originId="D9512DC3-C6B7-471A-ADE5-187595244D70" tscDM:project="vid">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="94750"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="2160" stDim:w="3840"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="zh-CN">Chinese</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Caption" xmpDM:frameRate="f1000" xmpDM:trackName="Captioning" stFnt:fontFamily="Arial" tscDM:fontSize="128" tscDM:bgOpacity="0.750000" tscDM:position="overlay">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="3917" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 正在执行4K视频测试}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="7917" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 请认真观看4K 60FPS 技术测试视频}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="11917" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 并注意设备的情况}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="15917" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 请在完成后向我们反馈此次观看的情况}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="19917" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 如果设备出现异常，请直接点击视频下方进行反馈}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="23917" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 没有异常请观看完，完成后自动切换到反馈页面}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="27917" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 如设备严重发热，卡顿掉帧，请一并反馈}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="31917" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 祝您测试愉快}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="62833" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 即将进行互动答题流畅测试，请准备}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="88000" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="zh-CN">{\\rtf1 测试即将结束，请准备进行反馈}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tsc:fgColor xmpG:red="255" xmpG:green="255" xmpG:blue="255"/><tsc:bgColor xmpG:red="0" xmpG:green="0" xmpG:blue="0"/></rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="1. 片头" xmpDM:startTime="0" tscDM:image="vid_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="2. 开始" xmpDM:startTime="11833" tscDM:image="vid_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 42"/></rdf:li><rdf:li><rdf:Description xmpDM:name="3. 即将结束" xmpDM:startTime="87917" tscDM:image="vid_Thumbnails.png" tscDM:imageindex="2" tscDM:imageoffset="0" tscDM:imagerect="150, 0, 75, 42"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="28169A96-A065-44E1-A59E-A2E8A5347737" tscIQ:authoredEmail="qNmU/NnNSFN+KRDaLne3MZlp+r6Rvxf8xOp9i9XO4Vk/Ko7b5P15nX9nxPHGoiFxuab/QcZKylvN\
+Ygsbc6mZ2l3wku0oxMneA8J+jWO5njD9yNZHLYeLrBjvgYJiCXsyXxKRZ0R76bEZbZSipNMmnRCs\
+ffbSJpOHbOQ+68NydgSywh4tstle1X86HXl8L9j/m6M5LpUpPu0g3j7x3crD/oiaidBMjnuO4ClV\
+B56Rsv611vnYpF3ZRszMHm2iUJYBusX9eYg8g1nQMvDoeqrNiIkHgRpPpun74WiybONT/wtCNVw0\
+lYrzARMfOCq+Eiive0SVkWJzR18Z86zv1S8bcQ==" tscIQ:requireUserId="0" tscIQ:locale="zh-CN" tscIQ:reportMethod="NONE" tscIQ:allowSkipQuiz="0" tscIQ:clientId="E7885279-24D1-4E90-AF2A-EF9A7A4E075B" tscIQ:hideReplay="0" tscIQ:quizHash="e016e5d162d8ee9db7d88878c4ce3ce7">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="67683" tscIQ:feedback="0" tscIQ:questionSetName="互动测试"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>请选择第一项</tscIQ:question><tscIQ:correctAnswer>0</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>选我</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>选这个也无所谓</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="上一个"/><rdf:li xmpDM:name="txtNext" xmpDM:value="下一步"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="立即进行测验"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="提交答案"/><rdf:li xmpDM:name="txtReview" xmpDM:value="重播最后一部分"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="查看答案"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="继续"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="true"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="true"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="true"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="right"/><rdf:li xmpDM:name="endaction" xmpDM:value="jumptourlnew"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="https://www.wjx.cn/vj/t2iF30E.aspx"/><rdf:li xmpDM:name="locale" xmpDM:value="zh-CN"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="https://aimg8.dlssyht.cn/u/1682698/ueditor/video/842/1682698/1629293863420767.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="vid_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="vid_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
