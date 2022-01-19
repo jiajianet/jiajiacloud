c41123ae0e3e266ff570d7746d1af9becf8b319f
jiajianet <69965213+jiajianet@users.noreply.github.com>
2022-01-19 15:05:23 +0800

    Update main.js

--- jcc/api/HDclass/assets/js/main.js
+++ jcc/api/HDclass/assets/js/main.js
@@ -127,7 +127,7 @@
 
 		// Toggle.
 			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+				'<a href="#navPanel" id="navPanelToggle">选择科目</a>'
 			)
 				.appendTo($wrapper);
 
