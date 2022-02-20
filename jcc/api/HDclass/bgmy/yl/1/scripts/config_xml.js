b88a349762d114cf0d187b329aac03afa9d0dfea
jiajianet <69965213+jiajianet@users.noreply.github.com>
2022-02-20 21:18:27 +0800

    BGMY back

---
+++ jcc/api/HDclass/bgmy/yl/1/scripts/config_xml.js
@@ -0,0 +1,50 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:creator="JiaJiaNet" dc:date="2021-02-10 04:24:05 下午" dc:source="Camtasia,19.0.10,chs" dc:title="final" tscDM:firstFrame="final_First_Frame.png" tscDM:originId="87EB2C1A-D6D2-4146-A1DA-A02AE831ED58" tscDM:project="final">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="455300"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="1920"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="zh-CN">Chinese</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="A7E5D63B-CB56-4CDD-9C77-86FF10A10355" tscIQ:authoredEmail="VAUDOTtrjhK5RZuF+5TZcLq3aqvyERswPHjM+zWERC52AcHhWBlLJgziyxA3sT4c6g8xlEjhvLbo\
+TemHJFCZx6F5pWhvod5mYcqY5b/vj0Sbq8brO5JRh8WIlTp1bwC54/LxeavVMocjGqS9Cfz2jmEJ\
+gu5EYlm+SLdP2j9RKKXcy8O7Rn9aVhSgWwp3PjU3lKJil0gjq4XjcNPtTXHBtWG5ZD7MTq9jf2PB\
+Diimk9mDpsiazgSjDaH3dLGCUDyEKUVpqzScVfq+ADCpGt7o9Tob9gpylKHDjarPZlhslhIHvVZc\
+qG4K2QzUKktT2HNxjBQhHb5h24YrLLQkH/CzFA==" tscIQ:requireUserId="0" tscIQ:locale="zh-CN" tscIQ:reportMethod="NONE" tscIQ:allowSkipQuiz="0" tscIQ:clientId="E7885279-24D1-4E90-AF2A-EF9A7A4E075B" tscIQ:hideReplay="1" tscIQ:quizHash="eed4d5c9661694dbf1f849203a30f299">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="32333" tscIQ:feedback="1" tscIQ:questionSetName="测验 1"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>准备好上课了么？</tscIQ:question><tscIQ:correctAnswer>1</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>OK</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>NO</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>继续</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li>请调整好状态，再进入课堂！</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="上一个"/><rdf:li xmpDM:name="txtNext" xmpDM:value="下一个"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="开始作答"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="提交答案"/><rdf:li xmpDM:name="txtReview" xmpDM:value="重播最后一部分"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="查看答案"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="继续"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="false"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="zh-CN"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="1612945801291349.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="final_First_Frame.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
