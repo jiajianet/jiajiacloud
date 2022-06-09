8624390138d472d700b2b92f196261ad8827aa72
jiajianet <69965213+jiajianet@users.noreply.github.com>
2022-06-09 22:10:59 +0800

    jccUpd

---
+++ jcc/desktoprun/menubuilder.js
@@ -0,0 +1,47 @@
+const { MenuItem, BrowserWindow } = require('electron')
+// const pkg = require('../package.json')
+
+/**
+* 构建菜单元素
+* @param {func} checkUpdate 检查更新回调,func(item,win)
+* @returns {object}
+*/
+module.exports = function () {
+  return {
+    version: new MenuItem({
+      label: `贾贾云课堂`
+    }),
+    refresh: new MenuItem({
+      label: '刷新',
+      click: function () {
+        Array.prototype.forEach.call(BrowserWindow.getAllWindows() || [], function (win) {
+          win.webContents.session.clearCache(() => {
+            win.webContents.session.clearHostResolverCache(() => {
+              win.reload()
+            })
+          })
+        })
+      }
+    }),
+    show: new MenuItem({
+      label: '显示',
+      click: function (item, win) {
+        Array.prototype.forEach.call(BrowserWindow.getAllWindows() || [], function (win) {
+          win.show()
+        })
+      }
+    }),
+    hidden: new MenuItem({
+      label: '隐藏',
+      click: function (item, win) {
+        Array.prototype.forEach.call(BrowserWindow.getAllWindows() || [], function (win) {
+          win.hide()
+        })
+      }
+    }),
+    quit: new MenuItem({
+      label: '退出',
+      role: 'quit'
+    })
+  }
+}
